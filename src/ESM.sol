52e987b0774f52f91dd3cdb7a70485210b32fff1
Lorenzo Manacorda <lorenzo@mailbox.org>
2019-05-13 17:09:43 +0200

    foo

--- src/ESM.sol
+++ src/ESM.sol
@@ -5,6 +5,8 @@ import "ds-note/note.sol";
 
 contract GemLike {
     function balanceOf(address) public returns (uint256);
+    function transfer(address, uint256) public returns (bool);
+    function transferFrom(address, address, uint256) public returns (bool);
 }
 
 contract EndLike {
@@ -13,17 +15,22 @@ contract EndLike {
 }
 
 contract ESM is DSAuth, DSNote {
-    uint256 public threshold;
-    mapping(address => uint256) public gems;
+    uint256 public cap;
     GemLike public gem;
-    EndLike end;
+    EndLike public end;
+    address public sun;
+
+    mapping(address => uint256) public gems;
+
     bool fired;
     bool freed;
     bool burnt;
 
-    constructor(address gem_, address end_, address owner_, address authority_) public {
+    constructor(address gem_, address end_, address sun_, uint256 cap_, address owner_, address authority_) public {
         gem = GemLike(gem_);
         end = EndLike(end_);
+        sun = sun_;
+        cap = cap_;
         owner = owner_;
         authority = DSAuthority(authority_);
     }
@@ -39,47 +46,55 @@ contract ESM is DSAuth, DSNote {
     }
 
     // -- admin --
-    function setThreshold(uint256 threshold_) external auth note {
-        threshold = threshold_;
+    function file(bytes32 job, address val) external auth note {
+        if (job == "end") end = EndLike(val);
+        if (job == "sun") sun = val;
     }
 
-    function aim(address end_) external auth note {
-        end = end_;
+    function file(bytes32 job, uint256 val) external auth note {
+        if (job == "cap") cap = val;
     }
 
+    // -- esm actions --
     function fire() public note {
-        require(!fired);
-        require(gem.balanceOf(address(this)) >= threshold, "es-module/threshold-not-met");
+        require(!fired, "esm/already-fired");
+        require(gem.balanceOf(address(this)) >= cap, "esm/cap-not-met");
 
         end.cage();
         fired = true;
     }
 
     function free() external auth note {
-        require(!burnt);
+        require(!burnt, "esm/already-burnt");
 
         freed = true;
     }
 
-    // TODO: maybe burner
     function burn() external auth note {
-        require(fired && !freed);
+        require(fired && !freed, "esm/too-early-to-burn");
 
-        gem.transfer(0x0, gem.balanceOf(address(this)));
         burnt = true;
+        bool ok = gem.transfer(address(sun), gem.balanceOf(address(this)));
+
+        require(ok, "esm/failed-transfer");
     }
 
+    // -- user actions --
     function join(uint256 wad) external note {
         require(!fired);
 
         gems[msg.sender] = add(gems[msg.sender], wad);
-        gem.transferFrom(msg.sender, address(this));
+        bool ok = gem.transferFrom(msg.sender, address(this), wad);
+
+        require(ok, "esm/failed-transfer");
     }
 
     function exit(address usr, uint256 wad) external note {
-        require(freed);
+        require(freed, "esm/not-freed");
 
         gems[msg.sender] = sub(gems[msg.sender], wad);
-        gem.transfer(usr, wad);
+        bool ok = gem.transfer(usr, wad);
+
+        require(ok, "esm/failed-transfer");
     }
 }
